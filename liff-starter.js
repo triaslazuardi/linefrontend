93c3d8b888682d60fe82a53fdbf8bcde14eb7265
triaslazuardi <trias.lazuardi@gmail.com>
2020-12-13 21:00:12 +0700

    part 5

--- liff-starter.js
+++ liff-starter.js
@@ -4,7 +4,7 @@ window.onload = function() {
  
     // DO NOT CHANGE THIS
     let myLiffId = "";
-    console.log("satu hohoho77");
+    console.log("satu hohoho88");
     // if node is used, fetch the environment variable and pass it to the LIFF method
     // otherwise, pass defaultLiffId
     if (useNodeJS) {
